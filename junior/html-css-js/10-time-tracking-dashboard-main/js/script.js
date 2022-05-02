76daff14ae4c5fed84d2e960e97d003dd00fe970
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-05-02 19:03:04 -0300

    função para pegar todos os item que tera mudança feito

--- junior/html-css-js/10-time-tracking-dashboard-main/js/script.js
+++ junior/html-css-js/10-time-tracking-dashboard-main/js/script.js
@@ -1,22 +1,41 @@
-const btns = document.querySelectorAll('.btnsData li')
+// const btns = document.querySelectorAll('.btnsData li')
 
-btns.forEach(btn => {
-  btn.addEventListener('click', handleClick)
-})
+// btns.forEach(btn => {
+//   btn.addEventListener('click', handleClick)
+// })
 
-function handleClick(e) {
-  const btnActive = document.querySelector('li.active')
-  const target = e.target
-  if(target !== btnActive) {
-    target.classList.add('active')
-    btnActive.classList.remove('active')
-  } 
-}
+// function handleClick(e) {
+//   const btnActive = document.querySelector('li.active')
+//   const target = e.target
+//   if(target !== btnActive) {
+//     target.classList.add('active')
+//     btnActive.classList.remove('active')
+//   } 
+//   fetchGet()
+// }
 
-function fetchGet() {
-  fetch('data.json')
-  .then(transformJson => transformJson.json())
-  .then(arr => {
-    return console.log(arr)
-  })
+
+// function selecionarElementosDeMudaca() {
+//   const arrH3 = document.querySelectorAll(".content h3 span")
+//   const arrP = document.querySelectorAll(".content p")
+//   console.log(arrH3, arrP)
+// }
+
+
+// function fetchGet(elementoClicado) {
+//   fetch('data.json')
+//   .then(transform => transform.json())
+//   .then(arr => {
+//     arr.forEach(obj => {
+//       const {daily, weekly, monthly} = obj.timeframes
+//       console.log(daily)
+//     })
+//   })
+//   selecionarElementosDeMudaca()
+// }
+
+class Testando {
+  constructor() {
+    
+  }
 }
