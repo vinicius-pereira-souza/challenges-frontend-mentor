ccb782566bbd3af61e4699b113f31d7de40e0c9c
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-08-21 16:06:25 -0300

    evento para alterar os dados do cartão em tempo real completo

--- junior/html-css-js/15-interactive-card-details-form-main/js/script.js
+++ junior/html-css-js/15-interactive-card-details-form-main/js/script.js
@@ -1,1 +1,40 @@
 // Cartão
+class Form {
+  constructor() {
+    this.cardNumber = document.querySelector('.cardFrontNum')
+    this.cardName = document.querySelector('.nomeCard')
+    this.cardDataMes = document.querySelector('span.mes')
+    this.cardDataAno = document.querySelector('span.ano')
+    this.numberCvc = document.querySelector('.numberBack')
+
+    this.inputs = document.querySelectorAll('input')
+
+    this.adionarEventos()
+  }
+
+  handleChangeInput = (e) => {
+    if(e.target.id === 'nomeCard') {
+      this.cardName.innerText = e.target.value
+
+    } else if(e.target.id === 'numeroCard') {
+      this.cardNumber.innerText = e.target.value
+
+    } else if(e.target.id === 'mes') {
+      this.cardDataMes.innerText = e.target.value
+
+    } else if(e.target.id === 'ano') {
+      this.cardDataAno.innerText = e.target.value
+
+    } else {
+      this.numberCvc.innerText = e.target.value
+    }
+  }
+
+  adionarEventos = () => {
+    this.inputs.forEach(input => {
+      input.addEventListener('keyup', this.handleChangeInput)
+    })
+  }
+}
+
+const formulario = new Form()
