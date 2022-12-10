dcbab4d5adc5b7c0ecc41890dc39bc6845f5ecc8
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-12-10 16:55:31 -0300

    validação do formulario completo

--- newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
+++ newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
@@ -1,18 +1,40 @@
-// const btn = document.getElementById('enviar')
-// const inputs = document.querySelectorAll('input')
+const btnSubmit = document.querySelector(".btnSubmit");
+const inputs = document.querySelectorAll("input");
 
-// function handleClick (event) {
-//   event.preventDefault()
-//   inputs.forEach((item) => {
-//     if(!item.checkValidity()) {
-//       item.style.borderColor = '#d86468'
-//       item.nextElementSibling.classList.add('invalido')
-//     } else {
-//       item.style.borderColor = '#74c8a4'
-//       item.nextElementSibling.classList.remove('invalido')
-//     }
-//   })
+btnSubmit.addEventListener("click", handleFormSubmit);
+inputs.forEach((input) => {
+  input.addEventListener("change", handleChangeInput);
+});
 
-// }
+function handleFormSubmit(e) {
+  e.preventDefault();
 
-// btn.addEventListener('click', handleClick)
+  inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      input.classList.add("errorActive");
+      input.nextElementSibling.classList.add("errorActive");
+      input.nextElementSibling.nextElementSibling.classList.add("errorActive");
+    } else {
+      input.classList.remove("errorActive");
+      input.nextElementSibling.classList.remove("errorActive");
+      input.nextElementSibling.nextElementSibling.classList.remove(
+        "errorActive",
+      );
+      input.value = "";
+    }
+  });
+
+  if (inputs[2].classList.contains("errorActive")) {
+    inputs[2].placeholder = "email@example/com";
+  }
+}
+
+function handleChangeInput(e) {
+  if (e.target.checkValidity()) {
+    e.target.classList.remove("errorActive");
+    e.target.nextElementSibling.classList.remove("errorActive");
+    e.target.nextElementSibling.nextElementSibling.classList.remove(
+      "errorActive",
+    );
+  }
+}
