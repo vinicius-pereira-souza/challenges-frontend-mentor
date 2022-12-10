fc022f6e315cc8d0e85ba6208442a85f7236ee5c
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-12-10 15:17:39 -0300

    html do desafio refatorado

--- newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
+++ newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
@@ -1,18 +1,18 @@
-const btn = document.getElementById('enviar')
-const inputs = document.querySelectorAll('input')
+// const btn = document.getElementById('enviar')
+// const inputs = document.querySelectorAll('input')
 
-function handleClick (event) {
-  event.preventDefault() 
-  inputs.forEach((item) => {
-    if(!item.checkValidity()) {
-      item.style.borderColor = '#d86468'
-      item.nextElementSibling.classList.add('invalido')
-    } else {
-      item.style.borderColor = '#74c8a4'
-      item.nextElementSibling.classList.remove('invalido')
-    }
-  })
-    
-}
+// function handleClick (event) {
+//   event.preventDefault()
+//   inputs.forEach((item) => {
+//     if(!item.checkValidity()) {
+//       item.style.borderColor = '#d86468'
+//       item.nextElementSibling.classList.add('invalido')
+//     } else {
+//       item.style.borderColor = '#74c8a4'
+//       item.nextElementSibling.classList.remove('invalido')
+//     }
+//   })
 
-btn.addEventListener('click', handleClick)
+// }
+
+// btn.addEventListener('click', handleClick)
