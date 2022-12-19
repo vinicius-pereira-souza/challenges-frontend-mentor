2e3bd69f12b42c9b793ea723f42529102a4679b7
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-12-19 15:41:04 -0300

    validação dos input utilizado regex completo

--- newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
+++ newbie/html-css-js/01-intro-component-with-signup-form-master/js/script.js
@@ -2,26 +2,21 @@ const btnSubmit = document.querySelector(".btnSubmit");
 const inputs = document.querySelectorAll("input");
 const messSuccess = document.querySelector(".succMessage");
 
-btnSubmit.addEventListener("click", handleFormSubmit);
 inputs.forEach((input) => {
   input.addEventListener("change", handleChangeInput);
 });
 
-function handleFormSubmit(e) {
+btnSubmit.addEventListener("click", handleClickSubmitForm);
+
+// valida os input apartir do click no botão
+function handleClickSubmitForm(e) {
   e.preventDefault();
 
   inputs.forEach((input) => {
     if (!input.checkValidity()) {
-      input.classList.add("errorActive");
-      input.nextElementSibling.classList.add("errorActive");
-      input.nextElementSibling.nextElementSibling.classList.add("errorActive");
+      activeErrorMessage(input);
     } else {
-      input.classList.remove("errorActive");
-      input.nextElementSibling.classList.remove("errorActive");
-      input.nextElementSibling.nextElementSibling.classList.remove(
-        "errorActive",
-      );
-
+      removeErrorMessage(input);
       messSuccess.classList.add("showSuccessSubmit");
       input.value = "";
       setInterval(() => {
@@ -29,18 +24,57 @@ function handleFormSubmit(e) {
       }, 2000);
     }
   });
-
-  if (inputs[2].classList.contains("errorActive")) {
-    inputs[2].placeholder = "email@example/com";
-  }
 }
 
 function handleChangeInput(e) {
-  if (e.target.checkValidity()) {
-    e.target.classList.remove("errorActive");
-    e.target.nextElementSibling.classList.remove("errorActive");
-    e.target.nextElementSibling.nextElementSibling.classList.remove(
-      "errorActive",
-    );
+  const validationEmail = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+
+  // validação para nome e sobrenome
+  if (e.target.id === "nome" || e.target.id === "sobrenome") {
+    e.target.value = clearInputName(e.target);
+
+    if (e.target.classList.contains("errorActive")) {
+      removeErrorMessage(e.target);
+    }
   }
+
+  // validação para email
+  if (e.target.id === "emailInput") {
+    if (!validationEmail.test(e.target.value)) {
+      activeErrorMessage(e.target);
+    } else {
+      removeErrorMessage(e.target);
+    }
+  }
+
+  // validação para senha
+  if (e.target.id === "senha") {
+    if (!e.target.checkValidity()) {
+      activeErrorMessage(e.target);
+    } else {
+      removeErrorMessage(e.target);
+    }
+  }
+}
+
+// remove digitos, . e espaços dos input do tipo texto
+function clearInputName(input) {
+  const clearInputText = /[\d.-\s]/g;
+  const cleanValue = input.value.replace(clearInputText, "");
+
+  return cleanValue;
+}
+
+// remove messagem de erro caso ela tenha ativa
+function removeErrorMessage(input) {
+  input.classList.remove("errorActive");
+  input.nextElementSibling.classList.remove("errorActive");
+  input.nextElementSibling.nextElementSibling.classList.remove("errorActive");
+}
+
+// adicionar a messagem de erro
+function activeErrorMessage(input) {
+  input.classList.add("errorActive");
+  input.nextElementSibling.classList.add("errorActive");
+  input.nextElementSibling.nextElementSibling.classList.add("errorActive");
 }
